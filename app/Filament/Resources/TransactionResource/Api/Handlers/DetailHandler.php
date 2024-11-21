84e098134cc90fbd73edeed9d14fe02194156618
Ega Bagus Purnama <egabagus1@gmail.com>
2024-11-21 13:36:03 +0700

    fix API detail

--- app/Filament/Resources/TransactionResource/Api/Handlers/DetailHandler.php
+++ app/Filament/Resources/TransactionResource/Api/Handlers/DetailHandler.php
@@ -20,42 +20,49 @@ class DetailHandler extends Handlers
 
     public function handler(Request $request)
     {
-        $id = $request->route('id');
+        try {
+            $id = $request->route('id');
 
-        $header = Transaction::with('details.item')->where('id', $id)->first();
+            $header = Transaction::with('details.item')->where('id', $id)->first();
 
-        if ($header) {
-            $cust = Customer::where('id', $header->cust_id)->select('id', 'code', 'name', 'phone', 'address')->first();
-            $paymment = PaymentMethod::where('id', $header->payment_id)->select('id', 'name')->first();
+            if ($header) {
+                $cust = Customer::where('id', $header->cust_id)->select('id', 'code', 'name', 'phone', 'address')->first();
+                $paymment = PaymentMethod::where('id', $header->payment_id)->select('id', 'name')->first();
 
-            //this is for API [get] show transaction
-            $data = [
-                'code' => $header->code,
-                'date' => Carbon::parse($header->date)->format('d-m-Y h:i:s'),
-                'total_amount' => $header->total_amount,
-                'payment_status' => $header->payment_status,
-                'payment' => $paymment,
-                'customer' => $cust ?? ""
-            ];
-
-            foreach ($header->details as $detail) {
-                $data['item'][] = [
-                    'item_name' => $detail->item->name,
-                    'quantity' => $detail->qty,
-                    'price' => $detail->price,
-                    'total_price' => $detail->total_price,
+                //this is for API [get] show transaction
+                $data = [
+                    'code' => $header->code,
+                    'date' => Carbon::parse($header->date)->format('d-m-Y h:i:s'),
+                    'total_amount' => $header->total_amount,
+                    'payment_status' => $header->payment_status,
+                    'payment' => $paymment,
+                    'customer' => $cust ?? ""
                 ];
-            }
 
+                foreach ($header->details as $detail) {
+                    $data['item'][] = [
+                        'item_name' => $detail->item->name,
+                        'quantity' => $detail->qty,
+                        'price' => $detail->price,
+                        'total_price' => $detail->total_price,
+                    ];
+                }
+
+                return response()->json([
+                    'status' => 'OK',
+                    'data' => $data
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'Not Found',
+                    'message' => 'Transaction Not Found'
+                ], 404);
+            }
+        } catch (\Throwable $th) {
             return response()->json([
-                'status' => 'OK',
-                'data' => $data
-            ]);
-        } else {
-            return response()->json([
-                'status' => 'Not Found',
-                'message' => 'Transaction Not Found'
-            ], 404);
+                'status' => 'Error',
+                'message' => $th->getMessage()
+            ], 500);
         }
     }
 }
